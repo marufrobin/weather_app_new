66948c4113c1e39124798978a89edd1cfa5cd72d
Maruf Robin <maruf.robin@northsouth.edu>
2022-12-10 18:35:57 +0600

    Forgot to add HTTP

---
+++ lib/custom_http.dart
@@ -0,0 +1,5 @@
+class CustomHttp {
+  String apiToken = "175cf6ab4459aa37983a566b55f6167f";
+  String url =
+      "https://api.openweathermap.org/data/2.5/weather?lat=37.4219983&lon=-122.084&units=metric&appid=";
+}
--- lib/screen/building_body.dart
+++ lib/screen/building_body.dart
@@ -1,14 +1,38 @@
 import 'package:flutter/material.dart';
 
-class BuildingBody extends StatelessWidget {
+class BuildingBody extends StatefulWidget {
   const BuildingBody({
     Key? key,
   }) : super(key: key);
 
   @override
+  State<BuildingBody> createState() => _BuildingBodyState();
+}
+
+class _BuildingBodyState extends State<BuildingBody> {
+  String apiToken = "175cf6ab4459aa37983a566b55f6167f";
+
+  addData() async {
+    String url =
+        "https://api.openweathermap.org/data/2.5/weather?lat=37.4219983&lon=-122.084&units=metric&appid=$apiToken";
+    var resonpce = await Uri.parse(url);
+    print("Responce: ${resonpce}");
+  }
+
+  @override
+  void initState() {
+    addData();
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Container(
+      height: double.infinity,
+      width: double.infinity,
       color: Colors.cyan,
+      child: Text("Maruf Ahmed Robin"),
     );
   }
 }
